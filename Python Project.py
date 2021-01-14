2cd18f1d7797614ff55305ae7dcfd9a7d8f204ff
Abhinav Mahesh <abhinav.mahesh6@gmail.com>
2021-01-14 22:44:30 +0530

    abhichanges

--- Python Project.py
+++ Python Project.py
@@ -22,7 +22,7 @@ def TravelID():
 def Getinputs():        
     mycon=sqltor.connect(host="localhost",user="root",passwd="mysql",database="Project",auth_plugin='mysql_native_password')
     cursor=mycon.cursor()
-    a=int(input('Enter 1 for one way and 2 for a roundtrip:'))
+    oneround=input("Enter 1 for one-way and 2 for round trip: ")
     LA=[]
     LC=[]
     LI=[]
@@ -60,61 +60,61 @@ def Getinputs():
             tempvar=100
             
             
-        file1=open(filename,'r')
-        read1=file1.read()
-        read1=int(read1)
-        Z=input("Enter plane_ID:")
-        for i in range(0,_NoAdults):
+        fileread=open(filename,'r')
+        travelidvar=fileread.read()#read1
+        travelidvar=int(travelidvar)
+        InpPlane_ID=input("Enter plane_ID from above: ")#Z
+        for inp1 in range(0,_NoAdults):
             name1=input("Enter Name:")
             age1=input("Enter Age:")
             gender1=input('Enter Gender(M/F/O):')
             passno1=input('Enter Passport Number:')
             meal1=input("Enter meal preferences:(Veg/Non-Veg):")
             print("\n",name1,age1,gender1,passno1,meal1)
-            travelid1=p[read1]
-            read1+=1
+            travelid1=p[travelidvar]
+            travelidvar+=1
             list1=[name1,passno1,gender1,age1,travelid1,meal1]
-            query3="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name1,passno1,gender1,age1,travelid1,meal1,Z)
+            query3="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name1,passno1,gender1,age1,travelid1,meal1,InpPlane_ID)
             cursor.execute(query3)
             mycon.commit()
             LA.append(list1)
-        for j in range(0,_NoChildren):
+        for inp2 in range(0,_NoChildren):
             name2=input("Enter Name:")
             age2=input("Enter Age:")
             gender2=input('Enter Gender(M/F/O):')
             passno2=input('Enter Passport Number:')
             meal2=input("Enter meal preferences:(Veg/Non-Veg):")
             print("\n",name2,age2,gender2,passno2,meal2)
-            travelid2=p[read1]
-            read1+=1
+            travelid2=p[travelidvar]
+            travelidvar+=1
             list2=[name2,passno2,gender2,age2,travelid2,meal2]
-            query4="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name2,passno2,gender2,age2,travelid2,meal2,Z)
+            query4="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name2,passno2,gender2,age2,travelid2,meal2,InpPlane_ID)
             cursor.execute(query4)
             mycon.commit()
             LC.append(list2)
-        for k in range(0,_NoInfants):
+        for inp3 in range(0,_NoInfants):
             name3=input("Enter Name:")
             age3=input("Enter Age:")
             gender3=input('Enter Gender(M/F/O):')
             passno3=input('Enter Passport Number:')
             meal3=input("Enter meal preferences:(Veg/Non-Veg):")
             print("\n",name3,age3,gender3,passno3,meal3)
-            travelid3=p[read1]
-            read1+=1
-            query5="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name3,passno3,gender3,age3,travelid3,meal3,Z)
+            travelid3=p[travelidvar]
+            travelidvar+=1
+            query5="insert into Cust_info values('%s','%s','%s','%s','%s','%s','%s')"%(name3,passno3,gender3,age3,travelid3,meal3,InpPlane_ID)
             cursor.execute(query5)
             mycon.commit()
             list3=[name3,passno3,gender3,age3,travelid3,meal3]
-            LA.append(list3)
+            LI.append(list3)
             
         mobile=int(input("Enter Mobile Number:"))
         email_ID=input("Enter Email_ID:")
         print("\n",mobile,email_ID)
-        file1.close()
-        file1=open(filename,'w')
-        read1=str(read1)
-        file1.write(read1)
-        file1.close()
+        fileread.close()
+        filewrite=open(filename,'w')
+        travelidvar=str(travelidvar)
+        filewrite.write(travelidvar)
+        filewrite.close()
 
     mycon.close()
 if __name__ == "__main__":
