1ea6f5cf319ab3ecccdbf57aa471c1e6e7598df4
Abhinav Mahesh <abhinav.mahesh6@gmail.com>
2021-01-24 16:45:45 +0530

    almost done

--- Python Project.py
+++ Python Project.py
@@ -2,12 +2,10 @@ import mysql.connector as sqltor
 import random
 import csv
 import time
-from datetime import datetime
 from datetime import date
-from datetime import timedelta
 filename = "c:/workspace/abhi_class_project/Aeroplane_Ctr.txt"
 global mycon
-mycon=sqltor.connect(host="localhost",user="root",passwd="secret",database="Project",auth_plugin="mysql_native_password")
+mycon=sqltor.connect(host="localhost",user="root",passwd="mysql",database="Project",auth_plugin="mysql_native_password")
 global listbookid
 listbookid=[]
 
@@ -19,7 +17,7 @@ def main():
     mycon.close()
 
 def TravelID():
-    global TravelList#p
+    global TravelList
     TravelList=[]
     for i in range(65,93):
         for j in range(0,10):
@@ -33,12 +31,6 @@ def TravelID():
                             TravelList.append(s)
 
 def Getinputs():             
-    global oneround
-    oneround=int(input("Enter 1 for one-way and 2 for round trip: "))
-    LA=[]
-    LC=[]
-    LI=[]
-    emplist1=[]
     tempvar=1
     global _Class
     global _NoAdults
@@ -49,25 +41,23 @@ def Getinputs():
     global email_ID
     global mobile
     while tempvar==1:
-        _From=input('Travelling From: ')#b
-        _To=input('Travelling To: ')#c
-        _Departure=input('Date of Departure(yyyy-mm-dd): ')#d
-        _NoAdults=int(input('Number of Adults(12+): '))#e
-        _NoChildren=int(input('Number of Children(2-12): '))#f
-        _NoInfants=int(input('Number of Infants(Under 2): '))#g
-        _Class=input('Class(Economy,Premium Economy,Business): ')#h
-        #print("\n",_From,_To,_Departure,_NoAdults,_NoChildren,_NoInfants,_Class)
+        _From=input('Travelling From: ')
+        _To=input('Travelling To: ')
+        _Departure=input('Date of Departure(yyyy-mm-dd): ')
+        _NoAdults=int(input('Number of Adults(12+): '))
+        _NoChildren=int(input('Number of Children(2-12): '))
+        _NoInfants=int(input('Number of Infants(Under 2): '))
+        _Class=input('Class(Economy,Premium Economy,Business): ')
         query1="select a.plane_id,a.Company,a.place_of_departure,a.Destination,ac.cost,time(a.Time_of_Dep) from aeroplane a,aeroplane_cost ac where a.plane_id=ac.plane_id and a.place_of_departure='%s' and a.destination='%s' and ac.class='%s'"%(_From,_To,_Class)
         cursor=mycon.cursor()
         cursor.execute(query1)
-        _Fetch1=cursor.fetchall()#Y
+        _Fetch1=cursor.fetchall()
         def displayplanes():
             print()
             print()
             print("These are the flights available between cities:")
             print()
             sep = "|"
-            #print("Plane_ID|Airline\t\t|City 1\t\t|City 2\t|Cost\t|Time of Departure|")
             _plane_id_h = "Plane Id"
             _airline_h = "Airline"
             _city1_h = "City 1"
@@ -107,12 +97,12 @@ def Getinputs():
                 
         
         global InpPlane_ID
-        InpPlane_ID=input("Select plane_ID from above: ")#Z
+        InpPlane_ID=input("Select plane_ID from above: ")
         for traverse2 in _Fetch1:
             if InpPlane_ID in traverse2:
                 time_of_dep = str(traverse2[-1]) 
         fileread=open(filename,'r')
-        travelidvar=fileread.read()#read1
+        travelidvar=fileread.read()
         travelidvar=int(travelidvar)
         
         for inp1 in range(0,_NoAdults):
@@ -121,45 +111,37 @@ def Getinputs():
             gender1=input('Enter Gender(M/F/O): ')
             passno1=input('Enter Passport Number(12 digits): ')
             meal1=input("Enter on flight meal preference:(Veg/Non-Veg): ")
-            #print("\n",name1,age1,gender1,passno1,meal1)
             travelid1=TravelList[travelidvar]
             travelidvar+=1
-            list1=[name1,passno1,gender1,age1,travelid1,meal1]
             query3="insert into Cust_info(Name,Passport_No,Gender,Age,Travel_ID,meal_pref,Plane_ID,Date_of_Dep,Time_of_Dep,BookingID,Class,Adult) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(name1,passno1,gender1,age1,travelid1,meal1,InpPlane_ID,_Departure+' 00:00:00',time_of_dep,BookingID,_Class,'Adult')
             cursor.execute(query3)
             mycon.commit()
-            LA.append(list1)
         for inp2 in range(0,_NoChildren):            
             name2=input("Enter Name: ")
             age2=input("Enter Age: ")
             gender2=input('Enter Gender(M/F/O): ')
             passno2=input('Enter Passport Number(12 digits): ')
             meal2=input("Enter on flight meal preferences:(Veg/Non-Veg): ")
-            #print("\n",name2,age2,gender2,passno2,meal2)
             travelid2=TravelList[travelidvar]
             travelidvar+=1
-            list2=[name2,passno2,gender2,age2,travelid2,meal2]
             query4="insert into Cust_info(Name,Passport_No,Gender,Age,Travel_ID,meal_pref,Plane_ID,Date_of_Dep,Time_of_Dep,BookingID,Class,Adult) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(name2,passno2,gender2,age2,travelid2,meal2,InpPlane_ID,_Departure+' 00:00:00',time_of_dep,BookingID,_Class,'Child')
             cursor.execute(query4)
             mycon.commit()
-            LC.append(list2)
         for inp3 in range(0,_NoInfants):
             name3=input("Enter Name: ")
             age3=input("Enter Age(in months): ")
             gender3=input('Enter Gender(M/F/O): ')
             passno3=input('Enter Passport Number: ')
             meal3="None"
-            #print("\n",name3,age3,gender3,passno3,meal3)
             travelid3=TravelList[travelidvar]
             travelidvar+=1
             query5="insert into Cust_info(Name,Passport_No,Gender,Age,Travel_ID,meal_pref,Plane_ID,Date_of_Dep,Time_of_Dep,BookingID,Class,Adult) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')"%(name3,passno3,gender3,age3+' months',travelid3,meal3,InpPlane_ID,_Departure+' 00:00:00',time_of_dep,BookingID,_Class,'Infant')
             cursor.execute(query5)
             mycon.commit()
-            list3=[name3,passno3,gender3,age3,travelid3,meal3]
-            LI.append(list3)
 
         mobile=int(input("Enter Mobile Number: "))
         email_ID=input("Enter Email_ID: ")
+        print()
         cost()
         query6="insert into Booking values('%s','%s','%s')"%(cardDetails(),BookingID,date.today())
         cursor.execute(query6)
@@ -180,19 +162,12 @@ def cost():
     for traverse6 in _Fetch3:
         for traverse7 in traverse6:
             cost=int(traverse7)
-    onewaycost=(_NoAdults*cost)+(_NoChildren*(cost/2))            
-    roundtripcost=onewaycost*2   
+    onewaycost=(_NoAdults*cost)+(_NoChildren*(cost/2))               
     global costvar
-    if oneround==1:
-        print()        
-        costvar="Total cost: "+str(round(onewaycost))
-        print(costvar)      
-        print()
-    elif oneround==2:
-        print()
-        costvar="Total cost: "+str(round(roundtripcost))
-        print(costvar)
-        print()
+    print()        
+    costvar="Total cost: "+str(round(onewaycost))
+    print(costvar)      
+    print()
 
 def bookingID():
     while True:
@@ -217,7 +192,6 @@ def cardDetails():
     return _name
 
 def printticket():
-    print("Booking Id:", BookingID)
     outfile = r'C:\workspace\abhi_class_project\printticket_' + BookingID + '.txt'
     _fh=open(outfile,'w')
     cursor=mycon.cursor()
@@ -227,7 +201,6 @@ def printticket():
     _Fetch2=cursor.fetchall()
     global d
     d={}
-    print(_Fetch2)
     rowcnt = 0
     for row in _Fetch2:
         rowcnt = rowcnt + 1
@@ -248,17 +221,15 @@ def printticket():
             meal_pref_h = "Meal Preference"
             sep = "|"
             _fh.write(passenger_h.ljust(30)+sep+travelid_h.ljust(20)+sep+age_h.ljust(5)+sep+gender_h.ljust(8)+sep+str(passport_h).ljust(15)+sep+meal_pref_h.ljust(20)+sep+"\n\n")
-
-            #_fh.write("Passenger(s)"+"Travel ID"+"Age"+"Gender"+"Passport Number"+"Meal Preference"+"\n\n")
+            
 
         _adult = row[10]+"("+str(d['Adult'])+")"
         _fh.write(_adult.ljust(30)+sep+row[14].ljust(20)+sep+row[13].ljust(5)+sep+row[12].ljust(8)+sep+str(row[11]).ljust(15)+sep+row[15].ljust(20)+sep+"\n")
-        #_fh.write(row[10]+"("+str(d['Adult'])+")"+row[14]+' '+row[13]+' '+row[12]+' '+str(row[11])+' '+row[15]+"\n") 
+         
         
         if rowcnt==length:
             _fh.write("\n"+costvar+"\n\n")
             _fh.write("Email Address: "+email_ID+"\n\n"+"Mobile Number: "+str(mobile)+"\n")
-    # Add other common text 
         
     _fh.close()
     
